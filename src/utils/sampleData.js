4a8743722f6a9f001868f22dc3041ab8ea58474c
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-05-07 14:10:35 -0400

    feature: sample data

---
+++ src/utils/sampleData.js
@@ -0,0 +1,23 @@
+export const sampleData = [
+  {
+    imgUrl: "/public/images/sampleImgs/stargate-sg1.jpg",
+    title: "Stargate SG1",
+    startYear: "1997",
+    endYear: "2007",
+    characters: ["Jack", "Daniel"],
+  },
+  {
+    imgUrl: "/public/images/sampleImgs/star-trek-original.png",
+    title: "Star Trek: The Original Series",
+    startYear: "1965",
+    endYear: "1969",
+    characters: ["Captain Kirk", "Spok"],
+  },
+  {
+    imgUrl: "/public/images/sampleImgs/star-trek-next-gen.png",
+    title: "Star Trek: The Next Generation",
+    startYear: "1987",
+    endYear: "1994",
+    characters: ["Commander Data", "Captain Picard"],
+  },
+];
